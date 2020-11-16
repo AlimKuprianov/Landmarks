2acabb3dd0c7a5ba56dabf4ca1c7459532ca08e8
AlimKuprianov <alimkosheev@gmail.com>
2020-11-17 00:23:49 +0300

    Add circleImage on ContentView

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,21 +10,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Hello, SwiftUI!")
-                .font(.title)
+        
+        VStack {
+            CircleImage()
             
-            
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+            VStack(alignment: .leading) {
+                
+                Text("Hello, SwiftUI!")
+                    .font(.title)
+                
+                
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
             }
+                
+            .padding()
         }
-            
-        .padding()
+        
+        
     }
 }
 
