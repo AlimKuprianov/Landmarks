a6f3313ea0e40657910331b68d2f40afaa72077e
AlimKuprianov <alimkosheev@gmail.com>
2020-11-17 00:17:45 +0300

    add CircleImage.swift

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Алим Куприянов on 17.11.2020.
+//  Copyright © 2020 Алим Куприянов. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("2020-01-31 17.06.06")
+            .frame(width: 300, height: 500)
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
