532186486447c93ea76b958c383fd7401d97fd84
AlimKuprianov <alimkosheev@gmail.com>
2020-11-17 22:14:24 +0300

    add new files

---
+++ LandmarksTwo/Landmark.swift
@@ -0,0 +1,43 @@
+//
+//  Landmark.swift
+//  LandmarksTwo
+//
+//  Created by Алим Куприянов on 17.11.2020.
+//  Copyright © 2020 Алим Куприянов. All rights reserved.
+//
+
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    fileprivate var imageName: String
+    fileprivate var coordinates: Coordinates
+    var state: String
+    var park: String
+    var category: Category
+
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+
+    enum Category: String, CaseIterable, Codable, Hashable {
+        case featured = "Featured"
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+    }
+}
+
+extension Landmark {
+    var image: Image {
+        ImageStore.shared.image(name: imageName)
+    }
+}
+
+struct Coordinates: Hashable, Codable {
+    var latitude: Double
+    var longitude: Double
+}
