72281b076ed14d667cb730fd4b084fe67f17f3ec
AlimKuprianov <alimkosheev@gmail.com>
2020-11-17 21:37:53 +0300

    resave

---
+++ LandmarksTwo/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  LandmarksTwo
+//
+//  Created by Алим Куприянов on 17.11.2020.
+//  Copyright © 2020 Алим Куприянов. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
