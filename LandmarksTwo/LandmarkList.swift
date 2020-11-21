7b81a639d778a56933939b86739f92c3d2a2587a
AlimKuprianov <alimkosheev@gmail.com>
2020-11-21 17:45:46 +0300

    add toggle to fav data

--- LandmarksTwo/LandmarkList.swift
+++ LandmarksTwo/LandmarkList.swift
@@ -12,16 +12,24 @@ struct LandmarkList: View {
     @State var showFavoritesOnly = false
     
     var body: some View {
-        NavigationView {
-            List(landmarkData) { landmark in
-                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
-                    LandmarkRow(landmark: landmark)
-                }
-            }
-            .navigationBarTitle(Text("Landmarks"))
-        }
-    }
-}
+           NavigationView {
+               List {
+                Toggle(isOn: $showFavoritesOnly, label: {
+                    Text("Favorites only")
+                })
+        
+                   ForEach(landmarkData) { landmark in
+                       if !self.showFavoritesOnly || landmark.isFavorite {
+                           NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                               LandmarkRow(landmark: landmark)
+                           }
+                       }
+                   }
+               }
+               .navigationBarTitle(Text("Landmarks"))
+           }
+       }
+   }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
