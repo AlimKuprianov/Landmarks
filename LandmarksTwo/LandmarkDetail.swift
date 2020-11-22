2b26b084ce567c22ecc49a9c4c0d6bc66b356f62
AlimKuprianov <alimkosheev@gmail.com>
2020-11-22 19:09:40 +0300

    save

--- LandmarksTwo/LandmarkDetail.swift
+++ LandmarksTwo/LandmarkDetail.swift
@@ -26,25 +26,27 @@ struct LandmarkDetail: View {
                 .frame(height: 300)
             
             CircleImage(image: landmark.image)
-                .offset(x: 0, y: -130)
+                .offset(y: -130)
                 .padding(.bottom, -130)
             
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
-                
-                
-                Button(action: {
-                    self.userData.landmarks[self.landmarkIndex].isFavorite.toggle()
-                }) {
-                    if self.userData.landmarks[self.landmarkIndex].isFavorite {
-                        Image(systemName: "star.fill")
-                            .foregroundColor(Color.yellow)
-                    } else {
-                        Image(systemName: "star")
-                            .foregroundColor(Color.gray)
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    
+                    Button(action: {
+                        self.userData.landmarks[self.landmarkIndex].isFavorite.toggle()
+                    }) {
+                        if self.userData.landmarks[self.landmarkIndex].isFavorite {
+                            Image(systemName: "star.fill")
+                                .foregroundColor(Color.yellow)
+                        } else {
+                            Image(systemName: "star")
+                                .foregroundColor(Color.gray)
+                        }
                     }
                 }
+                
                 
                 HStack(alignment: .top) {
                     Text(landmark.park)
