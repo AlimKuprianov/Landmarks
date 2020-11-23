286826d89672c865ca3afe22e96009f124c035ed
AlimKuprianov <alimkosheev@gmail.com>
2020-11-23 15:11:14 +0300

    create new file Badge

---
+++ LandmarksTwo/Badge.swift
@@ -0,0 +1,21 @@
+//
+//  Badge.swift
+//  LandmarksTwo
+//
+//  Created by Алим Куприянов on 23.11.2020.
+//  Copyright © 2020 Алим Куприянов. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
