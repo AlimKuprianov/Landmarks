be56789061804a17047b08e6f7d1fe0d7d1f5f01
AlimKuprianov <alimkosheev@gmail.com>
2020-11-24 22:39:13 +0300

    add badgeSymbol file

---
+++ LandmarksTwo/BadgeSymbol.swift
@@ -0,0 +1,39 @@
+//
+//  BadgeSymbol.swift
+//  LandmarksTwo
+//
+//  Created by Алим Куприянов on 24.11.2020.
+//  Copyright © 2020 Алим Куприянов. All rights reserved.
+//
+
+import SwiftUI
+
+struct BadgeSymbol: View {
+    var body: some View {
+        
+        GeometryReader { geometry in
+            Path { path in
+                let width = min(geometry.size.width, geometry.size.height)
+                let height = width * 0.75
+                let spacing = width * 0.030
+                let middle = width / 2
+                let topWidth = 0.226 * width
+                let topHeight = 0.488 * height
+                
+                path.addLines([
+                    CGPoint(x: middle, y: spacing),
+                    CGPoint(x: middle - topWidth, y: topHeight - spacing),
+                    CGPoint(x: middle, y: topHeight / 2 + spacing),
+                    CGPoint(x: middle + topWidth, y: topHeight - spacing),
+                    CGPoint(x: middle, y: spacing)
+                ])
+            }
+        }
+    }
+}
+
+struct BadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeSymbol()
+    }
+}
