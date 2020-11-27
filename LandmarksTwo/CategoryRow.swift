35928e34438a8aca6d21b2e0c7a8f21e7904b36c
AlimKuprianov <alimkosheev@gmail.com>
2020-11-27 16:34:05 +0300

    change body in categoryRow

--- LandmarksTwo/CategoryRow.swift
+++ LandmarksTwo/CategoryRow.swift
@@ -16,13 +16,24 @@ struct CategoryRow: View {
     
     
     var body: some View {
-        HStack(alignment: .top, spacing: 0) {
-            ForEach(self.items) { landmark in
-                Text(landmark.name)
+        VStack(alignment: .leading) {
+            Text(self.categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(self.items) { landmark in
+                        Text(landmark.name)
+                    }
+                }
             }
+            .frame(height: 85)
         }
     }
 }
+
 
 struct CategoryRow_Previews: PreviewProvider {
     static var previews: some View {
