9aebaab9d8190e0a39bc7b8e562778d47bccbc9d
AlimKuprianov <alimkosheev@gmail.com>
2020-11-27 16:24:59 +0300

    save

--- LandmarksTwo/CategoryHome.swift
+++ LandmarksTwo/CategoryHome.swift
@@ -9,12 +9,24 @@
 import SwiftUI
 
 struct CategoryHome: View {
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarkData,
+            by: { $0.category.rawValue }
+        )
+    }
     var body: some View {
-       NavigationView {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                ForEach(categories.keys.sorted(), id: \.self) { key in
+                    Text(key)
+                }
+            }
+            .navigationBarTitle(Text("Featured"))
         }
     }
 }
+
 
 struct CategoryHome_Previews: PreviewProvider {
     static var previews: some View {
