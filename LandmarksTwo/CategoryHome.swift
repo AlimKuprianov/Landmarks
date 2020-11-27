3fc3ad135ef33c3e6144ec7964732729df07e30b
AlimKuprianov <alimkosheev@gmail.com>
2020-11-27 15:44:38 +0300

    add new file

---
+++ LandmarksTwo/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  LandmarksTwo
+//
+//  Created by Алим Куприянов on 27.11.2020.
+//  Copyright © 2020 Алим Куприянов. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+       NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
